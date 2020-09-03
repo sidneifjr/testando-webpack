cd3cdc93422121d9e6e03fdbfc215741fb160b1b
sidneifjr <sidneijr.77@gmail.com>
2020-09-02 21:29:17 -0300

    alterando nome da pasta de produção

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,11 +8,11 @@ module.exports = merge(common, {
 
     output: {
         filename: 'main.[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
     },
 
     devServer: {
-        contentBase: './dist',
+        contentBase: './build',
     },
 
     devServer: {
